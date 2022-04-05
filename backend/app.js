461bbf9df7745be2cb99154120d163f27a0a8a8e
Giorgi Zardiashvili <zardiashviligiorgi17@gmail.com>
2022-04-05 09:44:14 +0400

    move user post from users to new register controller

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const cors = require('cors');
 const passport = require('passport');
 
-const { jwtCallback } = require('./auth/passport');
-const { adminGuard } = require('./guards/adminGuard');
+const {jwtCallback} = require('./auth/passport');
+const {adminGuard} = require('./guards/adminGuard');
 
 const accountRouter = require('./controllers/accounts');
 const categoryRouter = require('./controllers/categories');
@@ -14,6 +14,7 @@ const subscriptionRouter = require('./controllers/subscriptions');
 const transactionRouter = require('./controllers/transactions');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./auth/login');
+const registerRouter = require('./controllers/register');
 
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -22,17 +23,17 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(passport.initialize());
 
 const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
 };
 
 passport.use(new JwtStrategy(opts, jwtCallback));
 
-const auth = passport.authenticate('jwt', { session: false });
+const auth = passport.authenticate('jwt', {session: false});
 
 app.use('/accounts', auth, accountRouter);
 app.use('/categories', auth, categoryRouter);
@@ -42,5 +43,7 @@ app.use('/subscriptions', auth, subscriptionRouter);
 app.use('/transactions', auth, transactionRouter);
 app.use('/users', auth, adminGuard, usersRouter);
 app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+
 
 module.exports = app;
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -32,59 +32,6 @@ router.get('/:id?', (req, res) => {
     }
 });
 
-router.post(
-    '/register',
-    body('email').custom((value) => {
-        return User.find({
-            email: value,
-        }).then((user) => {
-            if (user.length > 0) {
-                return Promise.reject('Email already in use');
-            }
-        });
-    }),
-    body('password')
-        .isStrongPassword({
-            minLength: 6,
-            minLowercase: 1,
-            minUppercase: 1,
-            minNumbers: 1,
-            minSymbols: 0,
-        })
-        .withMessage(
-            'Password must be greater than 6 and contain at least one uppercase letter, one lowercase letter, and one number'
-        ),
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-            });
-        }
-
-        const body = req.body;
-
-        const user = new User({
-            email: body.email,
-            password: bcrypt.hashSync(body.password, 10),
-            role: body.role,
-            firstName: body.firstName,
-            lastName: body.lastName,
-            gender: body.gender,
-            dateOfBirth: body.dateOfBirth,
-            countryOfResidence: body.countryOfResidence,
-        });
-        user
-            .save()
-            .then((savedUser) => {
-                res.json(savedUser);
-            })
-            .catch((error) => {
-                console.error('The Promise is rejected!', error);
-            });
-    }
-);
-
 router.delete('/:id', (req, res) => {
     User.findByIdAndRemove(req.params.id).then(() => {
         res.status(204).end();
