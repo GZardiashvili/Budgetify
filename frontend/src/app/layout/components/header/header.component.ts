0c9e116b563d3bbf4a995f7dab26f38591941718
Giorgi Zardiashvili <zardiashviligiorgi17@gmail.com>
2022-04-06 20:10:04 +0400

    unsubscribe on destroy

--- frontend/src/app/layout/components/header/header.component.ts
+++ frontend/src/app/layout/components/header/header.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../../auth/services/auth.service';
-import { Router } from '@angular/router';
-import { UserService } from './services/user/user.service';
-import { Profile } from './models/profile';
-import { User } from './models/user';
-import { Observable } from 'rxjs';
+import {Component, OnDestroy} from '@angular/core';
+import {AuthService} from '../../../auth/services/auth.service';
+import {Router} from '@angular/router';
+import {UserService} from './services/user/user.service';
+import {Profile} from './models/profile';
+import {User} from './models/user';
+import {Observable, Subject} from 'rxjs';
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
+  private componentIsDestroyed$ = new Subject<boolean>();
   user: Observable<User> = this.userService.getUserProfile();
 
   profile: Profile = {
@@ -33,7 +35,7 @@ export class HeaderComponent {
     private authService: AuthService,
     private userService: UserService
   ) {
-    this.user.subscribe((user) => {
+    this.user.pipe(takeUntil(this.componentIsDestroyed$)).subscribe((user) => {
       this.profile.name = user.firstName + ' ' + user.lastName;
       this.profile.pfpUrl =
         user.pfpUrl == '' ? user.pfpUrl : '../../assets/default-pfp.png';
@@ -45,5 +47,10 @@ export class HeaderComponent {
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy() {
+    this.componentIsDestroyed$.next(true);
+    this.componentIsDestroyed$.complete();
   }
 }
